4f1be47df462e0b293efbad24d80f9f0cdc630fd
ksailens <ksailens@yandex.ru>
2018-10-15 18:28:13 +0300

    end of Second block

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Car/Car';
+import Car from "./Car/Car";
 
 class App extends Component {
   render() {
+    const divStyle = {
+      textAlign: 'center'
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={divStyle}>
+        <h1>Hello world!</h1>
+          <Car name={'Ford'} year={2018}/>
+            <p style={{color: 'blue'}}>COLOR</p>
+          <Car name="Audi" year={2016}>
+            <p style={{color: 'red'}}>COLOR</p>
+          </Car>
+          <Car name='Mazda' year={2010}/>
       </div>
     );
   }
---
+++ src/Car/Car.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// function car() {
+//     return (
+//         <h2>This is car component</h2>
+//     )
+// }
+
+// const car = () => {
+//     return (
+//         <div>This is car component</div>
+//     )
+// };
+// const car = () => (
+//     <div>
+//         This is car component
+//         <strong> Test</strong>
+//     </div>
+// )
+// export default car;
+
+export default props => (
+    <div>
+        <h3>Car name: {props.name}</h3>
+        <p>Number: <strong>{props.year}</strong></p>
+        { props.children }
+    </div>
+)
